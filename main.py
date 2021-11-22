78d0a466e7d136f688a6296ce16b0d39169483b1
matt <urb.maty@gmail.com>
2021-11-22 03:06:16 +0100

    Most of the node definition, heuristic function, making the grid

--- main.py
+++ main.py
@@ -16,3 +16,75 @@ PURPLE = (128, 0, 128)
 ORANGE = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
+class Node:
+    def __init__(self, row, column, width, row_sum):
+        self.row = row
+        self.column = column
+        self.x = row * width
+        self.y = column * width
+        self.color = WHITE
+        self.neigbors = []
+        self.width = width
+        self.row_sum = row_sum
+
+    def get_position(self):
+        return self.row, self.column
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == TURQUOISE
+
+    def reset(self):
+        return self.color == WHITE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def define_draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+
+
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j, gap, rows)
+            grid[i].append(node)
+
+
